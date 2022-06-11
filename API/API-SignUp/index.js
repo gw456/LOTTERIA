2abccac44d69a65f9cbbb605ba9453e05f1c2d61
Alma Maxfira <92255823+almaxfirab@users.noreply.github.com>
2022-06-11 19:22:25 +0700

    Add files via upload

---
+++ API/API-SignUp/index.js
@@ -0,0 +1,52 @@
+const express = require("express");
+const app = express();
+
+const admin = require("firebase-admin");
+const credentials = require("./serviceAccountKey.json");
+
+admin.initializeApp({
+    credential: admin.credential.cert(credentials)
+});
+
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT ${PORT}`);
+});
+
+app.post('/signup', async (req, res) => {
+    console.log(req.body);
+    const user = {
+        email: req.body.email,
+        password: req.body.password
+    }
+
+    const userResponse = await admin.auth().createUser({
+        email: user.email,
+        password: user.password,
+        emailVerified: false,
+        disabled: false
+    });
+    res.json(userResponse);
+});
+
+app.get('/signin', async (req, res) => {
+    console.log(req.body);
+    const user = {
+        email: req.body.email,
+        password: req.body.password
+    }
+
+    const userResponse = await admin.auth().createUser({
+        email: user.email,
+        password: user.password,
+        emailVerified: false,
+        disabled: false
+    });
+    res.json(userResponse);
+})
+
+
+
